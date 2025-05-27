0db592060c4c51c1786a692766d7773714c8df42
Nicolas Imagawa <nicimagawa@gmail.com>
2025-05-27 14:16:53 -0300

    added debugging prints

--- tests/test_extract_shipping_id.py
+++ tests/test_extract_shipping_id.py
@@ -9,5 +9,7 @@ from extraction.mercadolivre.script.get_seller_shipping_cost import get_shipping
 def test_shipping_extraction():
     test_results = get_shipping_id(test_run=True)
 
+    print("On test function:")
+    print(test_results["id"], type(test_results["id"]))
     assert test_results["id"] == os.environ["SHIPPING_ID_TEST_1"]
     assert test_results["list_cost"] == os.environ["LIST_COST"]
