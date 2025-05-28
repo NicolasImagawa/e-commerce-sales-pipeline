b1b4cdb4a962aa9b0aeee17555eff8defee4ba25
Nicolas Imagawa <nicimagawa@gmail.com>
2025-05-28 10:20:05 -0300

    changed dbt test python file

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -6,8 +6,8 @@ from psycopg2 import sql
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from dbt_files.install_deps import install_dependencies
-from dbt_files.unit_test import run_empty_shopee_fact_table
-from dbt_files.unit_test import  test_shopee_fact_table
+from dbt_files.prepare_testing import run_empty_shopee_fact_table
+from dbt_files.prepare_testing import  test_shopee_fact_table
 
 def dbt_unit_test():
     conn = psycopg2.connect(database = "postgres", user = "airflow", host= 'localhost', password = "airflow", port = 5432) #uses maintenance db to create the infrastructure
