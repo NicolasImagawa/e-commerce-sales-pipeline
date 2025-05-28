a141b785976566a4eb30b84e50eee0101e52504f
Nicolas Imagawa <nicimagawa@gmail.com>
2025-05-28 14:04:04 -0300

    changed test profile path

--- dbt_files/prepare_testing.py
+++ dbt_files/prepare_testing.py
@@ -12,7 +12,7 @@ def run_empty_shopee_tables(test_run):
     cli_args = [
                     "run",
                     "--select",
-                    "stg_shopee"
+                    "stg_shopee",
                     "entry_shopee",
                     "shopee_new_id",
                     "supplies",
